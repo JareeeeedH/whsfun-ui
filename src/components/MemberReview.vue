59a230564fcbd36562637384efa5b128ec118cc3
jared <jared_huang@mail.chungyo.net>
2022-03-17 01:36:41 +0800

    post留言評論排版

--- src/components/MemberReview.vue
+++ src/components/MemberReview.vue
@@ -3,43 +3,108 @@
     <div class="review">
       <div class="top-area">
         <div class="user-img">
-          <img src="https://fakeimg.pl/60x60/" alt="user-photo" />
+          <img :src="randomImg" alt="user-photo" />
         </div>
         <div class="general-msg">
-          <span>{{ review.speaker.name }} scored this whisky {{ review.points }} points</span>
+          <span>
+            <span class="name">{{ review.speaker.name }} </span>
+            scored this whisky
+            <span class="score">{{ review.points }} </span>
+            points
+          </span>
           <br />
-          <span>{{ review.date }}</span>
+          <span class="message-date">{{ review.date }}(GMT+0)</span>
         </div>
       </div>
       <div class="review-message">
         <h5 class="general-context">{{ review.content }}</h5>
-        <span class="nose-context">Nose:{{ review.nose }}</span>
-        <span class="taste-context">Taste:{{ review.taste }}</span>
-        <span class="review-context">Finish:{{ review.finish }}</span>
+        <span class="sub-context">
+          <span class="sub-context-title">Nose:</span>
+          <br />
+          {{ review.nose }}
+        </span>
+        <span class="sub-context">
+          <span class="sub-context-title">Taste:</span>
+          <br />
+          {{ review.taste }}
+        </span>
+        <span class="sub-context">
+          <span class="sub-context-title">
+            Finish:
+            <br />
+          </span>
+          {{ review.finish }}
+        </span>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import { computed } from "vue";
 export default {
   name: "memberReview",
   props: ["review"],
-  setup() {},
+  setup(props) {
+    let randomImg = computed(() => `https://picsum.photos/200/300?random=${props.review.speaker._id}`);
+    return { randomImg };
+  },
 };
 </script>
 
-<style>
+<style lang="scss">
 .review-content {
-  padding: 1rem;
-  border: 2px solid black;
+  padding: 1rem 2.5rem;
+  border: 1px solid black;
 }
 .top-area {
   display: flex;
 }
 
+.general-msg {
+  padding: 0.5rem;
+
+  .name {
+    font-weight: bold;
+    font-size: 1.25rem;
+  }
+  .score {
+    font-weight: bold;
+    font-size: 1.25rem;
+  }
+
+  .message-date {
+    font-size: 0.25rem;
+    color: grey;
+  }
+}
+
+.user-img {
+  width: 80px;
+  height: 80px;
+  border-radius: 50px;
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 50px;
+  }
+}
+
 .review-message {
   display: flex;
   flex-direction: column;
+
+  .general-context {
+    padding: 0.5rem 0;
+  }
+
+  .sub-context {
+    padding: 0.25rem 0;
+  }
+
+  .sub-context-title {
+    font-weight: bold;
+    color: rgb(79, 123, 245);
+  }
 }
 </style>
